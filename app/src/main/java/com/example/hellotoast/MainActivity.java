6d06d64c3e99a0d7199a434259e0cacade82fbf9
Toshiba <souhakhelifi1996@gmail.com>
2020-11-14 18:23:06 +0100

    hellotoast

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+    private EditText mMessageEditText;
+    public static final String EXTRA_MESSAGE =
+            "com.example.hellotoast.extra.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       mShowCount = (TextView) findViewById(R.id.show_count);
+       mMessageEditText = findViewById(R.id.show_count);
+    }
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+
+    }
+
+
+    /*public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }*/
+
+   public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
